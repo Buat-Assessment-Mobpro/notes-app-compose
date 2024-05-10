b7e351f5c6f16bc9ed5072f18fa66c3cdead2111
fahritech04 <fahricoding@gmail.com>
2024-05-10 19:05:07 +0700

    first commit

---
+++ app/src/main/java/com/example/notesappcompose/feature_note/presentation/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.notesappcompose.feature_note.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.notesappcompose.feature_note.navigation.AppNavHost
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppNavHost()
+        }
+    }
+}
+
+
